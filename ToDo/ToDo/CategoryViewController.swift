a8366dadb8825754379598661731c193e1a08311
milhet <benjamin.milhet@outlook.fr>
2022-12-30 01:07:54 +0100

    barre de recherche

--- ToDo/ToDo/CategoryViewController.swift
+++ ToDo/ToDo/CategoryViewController.swift
@@ -9,8 +9,10 @@ import UIKit
 
 class CategoryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var myCategoryTableView: UITableView!
+    @IBOutlet weak var myCategorySearchBar: UISearchBar!
     
     var myCategory: [Category] = []
+    var myRechercheCategory: [Category] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,28 +20,34 @@ class CategoryViewController: UIViewController, UITableViewDataSource, UITableVi
         self.myCategory.append(Category(_nom: "Travail"))
         self.myCategory.append(Category(_nom: "Maison"))
         
-        myCategoryTableView.dataSource = self
+        self.myRechercheCategory = self.myCategory
+        
+        self.myCategoryTableView.dataSource = self
         self.myCategoryTableView.delegate = self
+        self.myCategorySearchBar.delegate = self
 
         // Do any additional setup after loading the view.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.myCategory.count
+        return self.myRechercheCategory.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath) as! TableViewCell
         
-        cell.myNom.text = self.myCategory[indexPath.row].nom
+        cell.myNom.text = self.myRechercheCategory[indexPath.row].nom
         return cell
     }
     
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let configuration = UISwipeActionsConfiguration(actions: [UIContextualAction(style: .destructive, title: "Supprimer la ToDo", handler: {(action, view, completionHandler) in
             let row = indexPath.row
-            self.myCategory.remove(at: row)
+            let deleteCategory = self.myRechercheCategory[row]
+            self.myCategory = self.myCategory.filter(){$0 !== deleteCategory}
             completionHandler(true)
+            self.myRechercheCategory = self.myCategory
+            self.myCategorySearchBar.text = ""
             self.myCategoryTableView.reloadData()
         })])
         
@@ -49,8 +57,8 @@ class CategoryViewController: UIViewController, UITableViewDataSource, UITableVi
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let vc = segue.destination as? ViewController {
             let row = self.myCategoryTableView.indexPathForSelectedRow!.row
-            vc.myCategory = self.myCategory[row]
-            vc.myData = self.myCategory[row].myData
+            vc.myCategory = self.myRechercheCategory[row]
+            vc.myData = self.myRechercheCategory[row].myData
         }
     }
     
@@ -65,16 +73,11 @@ class CategoryViewController: UIViewController, UITableViewDataSource, UITableVi
         if let vc = unwindSegue.source as? AddCategoryViewController {
             if let addNewCategoryNom = vc.addNom {
                 self.myCategory.append(Category(_nom: addNewCategoryNom.text!))
+                self.myRechercheCategory = self.myCategory
             }
             self.myCategoryTableView.reloadData()
         }
         // Use data from the view controller which initiated the unwind segue
-    }
-    
-    @IBAction func deleteCategory(_ sender: UIButton) {
-        let row = sender.tag
-        self.myCategory.remove(at: row)
-        self.myCategoryTableView.reloadData()
     }
     
     /*
@@ -87,4 +90,22 @@ class CategoryViewController: UIViewController, UITableViewDataSource, UITableVi
     }
     */
 
+}
+
+extension CategoryViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.myRechercheCategory = []
+        
+        if searchText == "" {
+            self.myRechercheCategory = self.myCategory
+        } else {
+            for category in myCategory {
+                if category.nom.uppercased().contains(searchText.uppercased()) {
+                    self.myRechercheCategory.append(category)
+                }
+            }
+        }
+        
+        self.myCategoryTableView.reloadData()
+    }
 }
